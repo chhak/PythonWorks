e1b92a244720f5f29e6ee6890617e15a8019d319
nsaedae@gmail.com <nsaedae@gmail.com>
2021-02-23 20:39:36 +0900

    2021/02/23 Ch06 수업예제 추가

---
+++ Ch06/lib/Calc.py
@@ -0,0 +1,16 @@
+class Calc:
+
+    def plus(self, x, y):
+        return x + y
+
+    def minus(self, x, y):
+        return x - y
+
+    def multi(self, x, y):
+        return x * y
+
+    def div(self, x, y):
+        return x / y
+
+
+
