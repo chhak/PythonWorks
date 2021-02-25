aed683de4439e93f334ed423645e8ca8fc268a2d
nsaedae@gmail.com <nsaedae@gmail.com>
2021-02-25 18:57:54 +0900

    2021/02/25 교재예제 추가

---
+++ Exam1/1_1_Exam.py
@@ -0,0 +1,41 @@
+"""
+날짜 : 0000/00/00
+이름 : 홍길동
+내용 : 파이썬 연습문제 1.연산자
+"""
+x = 4
+y = -2
+
+z = x + y
+print('연산1 : x + y =', z)
+
+z = x - y
+print('연산2 : x - y =', z)
+
+z = x * y
+print('연산3 : x * y =', z)
+
+z = x / y
+print('연산4 : x / y =', z)
+
+z = (x + y) * (x - y)
+print('연산5 : (x + y) * (x - y) =', z)
+
+z = (x * y) + (x / y)
+print('연산6 : (x * y) + (x / y) =', z)
+
+z = x * y**2
+print('연산7 : %d x %d = %d' % (x, y**2, z))
+
+z = x * y**3
+print('연산8 : %d x %d = %d' % (x, y**3, z))
+
+z = x * y**4
+print('연산9 : %d x %d = %d' % (x, y**4, z))
+
+z = x * y**5
+print('연산10 : %d x %d = %d' % (x, y**5, z))
+
+
+
+
